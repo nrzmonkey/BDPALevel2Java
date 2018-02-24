cd941affc10ef05e3887242a25626f75debb5e09
BDPA20@metrostate.edu <BDPA20@metrostate.edu>
2018-02-24 13:55:26 -0600

    save my work

--- super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
+++ super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SuperHeroController {
     @RequestMapping("/superhero")
     public String greeting(Model model) {
-        model.addAttribute("name", "Super Hero Name");
-        model.addAttribute("power1", "A power that is super");
-        model.addAttribute("power2", "Another power that is super");
-        model.addAttribute("power3", "A third power that is super");
-        model.addAttribute("fact1", "This is fun fact is maybe not so fun");
-        model.addAttribute("fact2", "This is another fun fact");
-        model.addAttribute("fact3", "This is the third ");
+        model.addAttribute("name", " Stock Photo Steve");
+        model.addAttribute("power1", "Can make a verity of poses for your convenience");
+        model.addAttribute("power2", "Free with you're download of an adobe program");
+        model.addAttribute("power3", "Templates for lesons and used in meetings");
+        model.addAttribute("fact1", "In his photos he never sits");
+        model.addAttribute("fact2", "Wants to be in all you're office presentations");
+        model.addAttribute("fact3", "48 different images of steve");
 
         return "hero"; //Return "hero" to tell spring to use the hero.html template
     }
